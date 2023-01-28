2baaf2f2a14430c7d6ffe8720a36e9e595fd953b
flucloxin500 <110731277+flucloxin500@users.noreply.github.com>
2023-01-28 21:10:42 +0600

    Add files via upload

---
+++ 1059.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+
+    int x = 2, n;
+
+    while (x <= 100)
+    {
+        if ( x % 2 == 0)
+        printf("%d\n",x);
+        x+=1;
+    }
+    
+
+    
+
+return 0;
+}
